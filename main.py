9fd93fe03cca06079e93f85cdc2fe95d87ddc52b
pk3741 <przemek3741@gmail.com>
2023-01-15 23:22:43 +0100

    Added combine_points function, rectangle and sawtooth waveform generator

--- main.py
+++ main.py
@@ -5,14 +5,41 @@ from matplotlib import pyplot as plt
 
 
 def sin_gen(amplitude, frequency, phase, points):
-    sin_points = np.zeros([points,1])
+    sin_points = np.zeros([points, 1])
     for i in range(0, points):
-        sin_points[i] = amplitude * math.sin(2 * math.pi * frequency * i * 1/points + phase)
+        sin_points[i] = amplitude * math.sin(2 * math.pi * frequency * i * 1 / points + phase)
     return sin_points
 
-x = sin_gen(1,2, 0, 10)
 
-plt.plot(np.arange(0, 10), x)
+def rect_gen(amplitude, frequency, points):
+    rect_points = np.zeros([points, 1])
+    rect_points = amplitude * (sin_gen(amplitude, frequency, 0, points) > 0.0)
+    return rect_points
+
+
+def sawtooth_gen(amplitude, frequency, points):
+    sawtooth_points = np.zeros([points, 1])
+    for i in range(0, points):
+        sawtooth_points[i] = (i * 1 / points) % (1 / frequency) * frequency
+    return sawtooth_points
+
+
+def combine_signals(signal_a, signal_b, points):
+    combined_points = np.zeros([points, 1])
+    for i in range(0, points):
+        combined_points[i]= signal_a[i] * signal_b[i]
+    return combined_points
+
+
+
+actual_signal = np.zeros([1000, 1])
+sawtooth_signal = np.ones([1000, 1])
+sinus_signal = sin_gen(1, 2, 0, 1000)
+
+for i in range(0, 1000):
+    actual_signal[i] = sawtooth_signal[i] * sinus_signal[i]
+
+plt.plot(np.arange(0, 1000), actual_signal)
 plt.show()
 
 # root = Tk()
